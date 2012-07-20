56d8278a28e3ff46d4a3cd5f25a34a266f8aff74
Hayden Muhl <haydenmuhl@yahoo.com>
2012-07-19 19:22:38 -0700

    Simple windowed application

--- src/fractal/Main.java
+++ src/fractal/Main.java
@@ -1,7 +1,29 @@
 package fractal;
 
+import javax.swing.*;
+
 public class Main {
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("It works!");
+        frame.getContentPane().add(label);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
     public static void main(String[] args) {
-        System.out.println("It works!");
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
 }
