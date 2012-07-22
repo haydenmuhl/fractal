c178b54da2070090baf6a9a709935d8a9fc53e8a
Hayden Muhl <haydenmuhl@yahoo.com>
2012-07-21 17:46:13 -0700

    Orientation component done

--- src/fractal/Main.java
+++ src/fractal/Main.java
@@ -1,5 +1,7 @@
 package fractal;
 
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class Main {
@@ -7,13 +9,19 @@ public class Main {
         //Create and set up the window.
         JFrame frame = new JFrame("HelloWorldSwing");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //Add the ubiquitous "Hello World" label.
-        JLabel label = new JLabel("It works!");
-        frame.getContentPane().add(label);
-
-        //Display the window.
-        frame.pack();
+        frame.setLayout(new FlowLayout());
+        frame.setSize(new Dimension(700, 500));
+        
+        frame.add(new JButton("one"));
+        frame.add(new JButton("two"));
+        
+        Orientation c = new Orientation();
+        c.setSize(new Dimension(300, 300));
+        frame.add(c);
+        
+        frame.add(new JLabel("three"));
+        
+//        frame.pack();
         frame.setVisible(true);
     }
 
---
+++ src/fractal/Orientation.java
@@ -0,0 +1,81 @@
+package fractal;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class Orientation extends Component {
+    
+    private int[] xCoords = {10, 10, 50, 50, 20, 20};
+    private int[] yCoords = {10, 90, 90, 80, 80, 10};
+    
+    private boolean switchX = false;
+    private boolean switchY = false;
+    private boolean transpose = false;
+    
+    public Orientation() {
+        super();
+        enableEvents(AWTEvent.MOUSE_EVENT_MASK);
+    }
+    
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(100, 100);
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        Dimension d = g.getClipBounds().getSize();
+        int x = d.width;
+        int y = d.height;
+        
+        g.setColor(Color.white);
+        g.fillRect(0, 0, x, y);
+        
+        g.setColor(Color.black);
+        g.fillPolygon(xCoords, yCoords, xCoords.length);
+    }
+    
+    @Override
+    protected void processMouseEvent(MouseEvent e) {
+        if (e.getID() == MouseEvent.MOUSE_CLICKED) {
+            if (e.getButton() == MouseEvent.BUTTON3) {
+                mirror();
+            }
+            if (e.getButton() == MouseEvent.BUTTON1) {
+                rotate();
+            }
+            repaint();
+        }
+    }
+    
+    private void rotate() {
+        int[] coordArray = xCoords;
+        xCoords = yCoords;
+        yCoords = coordArray;
+        for (int i = 0; i < xCoords.length; i++) {
+            xCoords[i] = 100 - xCoords[i];
+        }
+        
+        boolean t = transpose;
+        boolean x = switchX;
+        boolean y = switchY;
+        
+        transpose = !t;
+        switchY = !t ^ y;
+        switchX = t ^ x;
+    }
+    
+    private void mirror() {
+        int[] coordArray;
+        if (transpose) {
+            coordArray = yCoords;
+        } else {
+            coordArray = xCoords;
+        }
+        for (int i = 0; i < coordArray.length; i++) {
+            coordArray[i] = 100 - coordArray[i];
+        }
+        
+        switchX = !switchX;
+    }
+}
